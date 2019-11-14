6848d8114c0d969e39662b11a81316934bd25017
zhibi <1017866168@qq.com>
2019-11-14 17:38:46 +0800

    update 修改grade的value的类型

--- src/utils/enum.js
+++ src/utils/enum.js
@@ -1,10 +1,10 @@
 const grade = [
   { label: '全部', value: '' },
-  { label: '幼儿园', value: '0' },
-  { label: '小学', value: '1' },
-  { label: '初中', value: '2' },
-  { label: '中职', value: '3' },
-  { label: '高中', value: '4' }
+  { label: '幼儿园', value: 0 },
+  { label: '小学', value: 1 },
+  { label: '初中', value: 2 },
+  { label: '中职', value: 3 },
+  { label: '高中', value: 4 }
 ]
 export default {
   grade: grade,
